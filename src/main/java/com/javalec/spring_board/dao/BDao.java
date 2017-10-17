48f176088dfc86fdbc397e42ca9d564a2dd0ff0c
flwkemd <flwkemd@naver.com>
2017-10-17 04:12:01 +0200

    implemented design & list form

---
+++ src/main/java/com/javalec/spring_board/dao/BDao.java
@@ -0,0 +1,17 @@
+package com.javalec.spring_board.dao;
+
+import java.util.ArrayList;
+
+import com.javalec.spring_board.dto.BDto;
+
+public class BDao {
+
+	public ArrayList<BDto> list() {
+		
+		ArrayList<BDto> dtos = null;
+		
+		return dtos;
+		
+		
+	}
+}
